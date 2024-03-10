09c6d04019020314566af208a2345414c5a6ebd8
Mattia Di Eleuterio <madiele92@gmail.com>
2024-03-10 10:58:44 +0100

    provider abstraction

--- src/text_to_speach/provider/google.rs
+++ src/text_to_speach/provider/google.rs
@@ -1,16 +1,58 @@
+use std::str::FromStr;
+
+use locale_codes::language::LanguageInfo;
 use tokio::{self, task};
 use tts_rust::{languages::Languages, tts::GTTSClient};
 
-async fn run(text: String, filename: String) {
-    let _ = task::spawn_blocking(move || {
-        let narrator: GTTSClient = GTTSClient {
+use super::{TtsCapabilites, TtsClient, TtsClientBuilder};
+
+pub struct GTTSClientBuilder {
+    volume: f32,
+    language: Languages,
+}
+
+impl TtsClientBuilder for GTTSClientBuilder {
+    fn capabilities() -> &'static [TtsCapabilites] {
+        &[TtsCapabilites::LanguageChoice]
+    }
+
+    fn default() -> Self {
+        Self {
             volume: 1.0,
-            language: Languages::English, // use the Languages enum
+            language: Languages::English,
+        }
+    }
+
+    fn authorize(self) -> Self {
+        panic!()
+    }
+
+    fn with_voice(self, _voice: String) -> Self {
+        panic!()
+    }
+
+    fn for_language(mut self, language: &LanguageInfo) -> Self {
+        self.language =
+            Languages::from_str(language.short_code.to_owned().unwrap().as_str()).unwrap();
+        self
+    }
+
+    fn build(self) -> GTTSClient {
+        GTTSClient {
+            volume: self.volume,
+            language: self.language,
             tld: "com",
-        };
-        let _ = narrator.save_to_file(text.as_str(), filename.as_str());
-    })
-    .await;
+        }
+    }
+}
+
+impl TtsClient for GTTSClient {
+    async fn speak_to_file(self, text: String, path: String) {
+        let _ = task::spawn_blocking(move || {
+            let _ = self.save_to_file(text.as_str(), path.as_str());
+        })
+        .await;
+    }
 }
 
 #[cfg(test)]
@@ -18,8 +60,36 @@ mod test {
     use super::*;
 
     #[tokio::test]
-    async fn google_test() {
-        run("ciao ciao".to_owned(), "test_gtts.mp3".to_owned()).await
+    async fn google_test_it() {
+        let client = GTTSClientBuilder::default()
+            .for_language(locale_codes::language::lookup("it").unwrap())
+            .build();
+
+        client
+            .speak_to_file("ciao ciao ciao".to_owned(), "test.it.mp3".to_owned())
+            .await;
+    }
+
+    #[tokio::test]
+    async fn google_test_en() {
+        let client = GTTSClientBuilder::default()
+            .for_language(locale_codes::language::lookup("en").unwrap())
+            .build();
+
+        client
+            .speak_to_file("hello hello hello".to_owned(), "test.en.mp3".to_owned())
+            .await;
+    }
+
+    #[tokio::test]
+    async fn google_test_es() {
+        let client = GTTSClientBuilder::default()
+            .for_language(locale_codes::language::lookup("es").unwrap())
+            .build();
+
+        client
+            .speak_to_file("hola hola hola".to_owned(), "test.es.mp3".to_owned())
+            .await;
     }
 }
 
--- src/text_to_speach/provider/mod.rs
+++ src/text_to_speach/provider/mod.rs
@@ -1,2 +1,24 @@
-mod openai;
 mod google;
+mod openai;
+
+#[trait_variant::make(HttpService: Send)]
+pub trait TtsClient {
+    async fn speak_to_file(self, text: String, path: String);
+}
+
+pub trait TtsClientBuilder {
+    fn capabilities() -> &'static [TtsCapabilites];
+    fn default() -> impl TtsClientBuilder;
+    fn authorize(self) -> impl TtsClientBuilder;
+    fn with_voice(self, voice: String) -> impl TtsClientBuilder;
+    fn for_language(self, language: &locale_codes::language::LanguageInfo)
+        -> impl TtsClientBuilder;
+    fn build(self) -> impl TtsClient;
+}
+
+pub enum TtsCapabilites {
+    LanguageChoice,
+    VoiceChoice,
+    RequiresAuth,
+}
+
